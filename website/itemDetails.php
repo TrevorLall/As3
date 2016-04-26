7839e991f7c5f7e29a0be1efefc35c57aca43354
GuthixJr <cars4321@hotmail.com>
2016-04-26 10:55:59 -0400

    Update itemDetails.php

--- website/itemDetails.php
+++ website/itemDetails.php
@@ -6,9 +6,32 @@
 <body>
 <form method="POST">
 	<?php 
-		if(isset($_SESSION['itemId']))
+	require 'DbTest.php';
+		if(isset($_SESSION['Itemid']))
 		{
-			
+			$data = $conn->query("SELECT * FROM inv_items WHERE item_id = '$_SESSION[Itemid]'");
+			$item = $data->fetch_assoc();
+			?><h3 style="underline"><?php $item['item_name']?></h3><?php
+			echo '<img src="data:image/jpeg;base64,'.base64_encode($item['item_image']).'" height="150" width="100" /><br />';
+			echo "Item Price: " . $item["item_price"] . "<br />";
+			echo "Quantity remaining: " . $item["item_qoh"] . "<br />";
+			echo "Description of Item: " . $item["item_desc"] . "<br />"; 
+			$endDate = strtotime($item["sale_end"]);
+			$remaining = $endDate - time();
+			if($remaining > 0)
+			{
+				$days_remain = floor($remaining / 86400);
+				$hours_remain = floor(($remaining % 86400) / 3600);
+				echo "Time left on sale: $days_remain days and $hours_remain hours.<br />";
+			}
+			else
+			{
+				echo "Sale has ended!";
+			}
+		}
+		else
+		{
+			header("location: mainPage.php");
 		}
 	?>
 </form>
